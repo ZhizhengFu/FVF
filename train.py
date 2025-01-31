c6e72cccdd6c5c05ea81d2cb2208e8095b009848
ZhizhengFu <zhizheng.foo@gmail.com>
2025-01-31 06:22:42 +0100

    add train.py and tomllib

---
+++ train.py
@@ -0,0 +1,20 @@
+import wandb
+import tomllib
+
+with open("config.toml", "rb") as f:
+    data = tomllib.load(f)
+
+# 1. Start a new run
+run = wandb.init(project="gpt4")
+
+# 2. Save model inputs and hyperparameters
+config = run.config
+config.dropout = 0.01
+
+# 3. Log gradients and model parameters
+run.watch(model)
+for batch_idx, (data, target) in enumerate(train_loader):
+    ...
+    if batch_idx % args.log_interval == 0:
+        # 4. Log metrics to visualize performance
+        run.log({"loss": loss})

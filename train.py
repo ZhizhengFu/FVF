634a85f59dcf2a2309fc3f243499945497a76417
ZhizhengFu <zhizheng.foo@gmail.com>
2025-02-21 17:34:41 +0800

    update config

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from core.config import Config
 from core.engine import Trainer
 
+CONFIG_NAME = "usrnet"
 
 def main():
-    config = Config("configs/config.toml")
+    config = Config(f"configs/{CONFIG_NAME}.toml")
     trainer = Trainer(config)
     trainer.run_loop()
 

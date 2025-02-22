3eae15fca2172813fc5685fedf89b5fc1f5bb26f
ZhizhengFu <zhizheng.foo@gmail.com>
2025-02-22 15:42:08 +0800

    add save_code_snapshot | GradualWarmupScheduler

--- core/engine/trainer.py
+++ core/engine/trainer.py
@@ -1,15 +1,13 @@
 from core.config import Config
-from core.utils import get_cur_time, create_experiment_directory, init_wandb, init_seed
+from core.utils import get_cur_time, init_wandb, init_seed, save_code_snapshot
 
 
 class Trainer:
     def __init__(self, config: Config):
         self.config: Config = config
-        init_seed(self.config["train"]["seed"])
-        if self.config["train"]["is_save"]:
-            create_experiment_directory(
-                self.config["model"]["name"], self._generate_dir_name()
-            )
+        init_seed(self.config["train"]["seed"], self.config["train"]["deterministic"])
+        if self.config["train"]["save_snapshots"]:
+            save_code_snapshot(self.config["model"]["name"], self._generate_dir_name())
         if self.config["train"]["wandb"]["is_enabled"]:
             init_wandb(
                 self.config["train"]["wandb"]["project"], self._generate_wandb_name()

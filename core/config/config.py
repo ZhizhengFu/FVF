b99c787c10af4f98fc9148ea56832421041e183b
ZhizhengFu <zhizheng.foo@gmail.com>
2025-02-23 21:48:28 +0800

    toml: missing_key and '' value

--- core/config/config.py
+++ core/config/config.py
@@ -1,23 +1,34 @@
 import tomllib
+import warnings
 
 
 class Config(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for key, value in self.items():
-            if isinstance(value, dict):
-                self[key] = Config(value)
+        self.update(
+            {
+                key: Config(value)
+                if isinstance(value, dict)
+                else None
+                if value == ""
+                else value
+                for key, value in self.items()
+            }
+        )
 
     def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{name}'"
-            )
+        return self[name]
 
     def __setattr__(self, name, value):
         self[name] = Config(value) if isinstance(value, dict) else value
+
+    def __missing__(self, key):
+        warnings.warn(
+            f"Key '{key}' is missing in the configuration. Returning None.",
+            UserWarning,
+            stacklevel=3,
+        )
+        return None
 
     @classmethod
     def from_toml(cls, config_path: str):
@@ -28,4 +39,5 @@ class Config(dict):
 
 if __name__ == "__main__":
     config = Config.from_toml("configs/usrnet.toml")
-    print(config.datasets.train.scales)
+    print(config.model.pretrained_path) # return None
+    print(config.model.pretrained_pat)  # return Warning + None
